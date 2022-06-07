7770a44901e4babc00f81cb86b0e3fe5d7caae7f
Liz-electro <miroshnik.liza@gmail.com>
2022-06-07 13:08:59 +0300

    test GitHub actions

--- ReadMe.md
+++ ReadMe.md
@@ -1,28 +1,31 @@
 # Testing framework
 
-
 ## Technologies used
+
 - JavaScript
 - [Cypress](https://www.cypress.io/)
 - Mochawesome
 
 ### Description
+
 Tests were written with Cypress.
 Located:
 ```./cypress/```
 
-
 ## How to run Cypress tests in cmd
+
 ```sh
-./node_modules/.bin/cypress run  
+./node_modules/.bin/cypress run --env "username={$email},password={$password}"   
 ```
 
 ## How to run Cypress tests in Browser
+
 ```sh
-./node_modules/.bin/cypress open  
+./node_modules/.bin/cypress open --env "username={$email},password={$password}"
 ```
 
 ## Reporting
+
 /reports/...
 
 
--- cypress/e2e/ui/test.cy.js
+++ cypress/e2e/ui/test.cy.js
@@ -13,11 +13,21 @@ describe('test', () => {
         const username = Cypress.env('username')
         const password = Cypress.env('password')
         login.loginAs(username, password);
+        cy.location("pathname").should("equal", "/sales");
     })
 
-    it('User should get an exeption in case of not valid password', () => {
+    it('User should get an exception in case of not valid password', () => {
         const username = Cypress.env('username')
         const password = Cypress.env('incorrectPassword')
         login.loginAs(username, password);
+    })
+
+    it('User should have a possibility to logout', () => {
+        const username = Cypress.env('username')
+        const password = Cypress.env('password')
+        login.loginAs(username, password);
+        cy.get("[data-testid=\"userInfoContainer\"]").click();
+        cy.get("#logout").click();
+        cy.location("pathname").should("equal", "/");
     })
 })
